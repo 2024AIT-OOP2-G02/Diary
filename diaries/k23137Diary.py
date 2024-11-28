df76b5004494c12e571ac752873c103c9c6d835e
k23137 <k23137kk@aitech.ac.jp>
2024-11-28 15:53:01 +0900

    変更

---
+++ diaries/k23137Diary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class PimanDiary(AbstractDiary):
+
+    def get_date(self):
+        return "2024-11-28"
+
+    def get_summary(self):
+        return "Git hub の操作に慣れない．"
+
+    def get_author(self):
+        return "K23137 横山幸咲"
